bc7c94415c42d8d19b587531b303df1b4c259bc5
Markelow Maxim <max.markelow@gmail.com>
2015-03-21 23:29:25 +0400

    Added CardioTimer and other features for sessionVM

---
+++ CardMonitor/Core/Repository/Monitor/MonitorRepository.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using CardioMonitor.Core.Models.Session;
+
+namespace CardioMonitor.Core.Repository.Monitor
+{
+    public class MonitorRepository
+    {
+        private static MonitorRepository _instance;
+        private static readonly object SyncObject = new object();
+        
+        private readonly List<PatientParams> _patientParams;
+
+        private int _index;
+
+        private int Index
+        {
+            get
+            {
+                if (_index + 1 > _patientParams.Count)
+                {
+                    _index = 0;
+                }
+                return _index++;
+            }
+            
+        }
+
+        private MonitorRepository()
+        {
+            _patientParams = new List<PatientParams>
+            {
+                new PatientParams
+                {
+                    AverageArterialPressure = 100,
+                    DiastolicArterialPressure = 80,
+                    HeartRate = 70,
+                    InclinationAngle = 0,
+                    RepsirationRate = 40,
+                    Spo2 = 40,
+                    SystolicArterialPressure = 120
+                },
+                new PatientParams
+                {
+                    AverageArterialPressure = 100,
+                    DiastolicArterialPressure = 80,
+                    HeartRate = 70,
+                    InclinationAngle = 10.5,
+                    RepsirationRate = 40,
+                    Spo2 = 40,
+                    SystolicArterialPressure = 120
+                },
+                new PatientParams
+                {
+                    AverageArterialPressure = 100,
+                    DiastolicArterialPressure = 80,
+                    HeartRate = 70,
+                    InclinationAngle = 21,
+                    RepsirationRate = 40,
+                    Spo2 = 40,
+                    SystolicArterialPressure = 120
+                },
+                new PatientParams
+                {
+                    AverageArterialPressure = 100,
+                    DiastolicArterialPressure = 80,
+                    HeartRate = 70,
+                    InclinationAngle = 30,
+                    RepsirationRate = 40,
+                    Spo2 = 40,
+                    SystolicArterialPressure = 120
+                },
+                new PatientParams
+                {
+                    AverageArterialPressure = 100,
+                    DiastolicArterialPressure = 80,
+                    HeartRate = 70,
+                    InclinationAngle = 21,
+                    RepsirationRate = 40,
+                    Spo2 = 40,
+                    SystolicArterialPressure = 120
+                },
+                new PatientParams
+                {
+                    AverageArterialPressure = 100,
+                    DiastolicArterialPressure = 80,
+                    HeartRate = 70,
+                    InclinationAngle = 10.5,
+                    RepsirationRate = 40,
+                    Spo2 = 40,
+                    SystolicArterialPressure = 120
+                },
+                new PatientParams
+                {
+                    AverageArterialPressure = 100,
+                    DiastolicArterialPressure = 80,
+                    HeartRate = 70,
+                    InclinationAngle = 0,
+                    RepsirationRate = 40,
+                    Spo2 = 40,
+                    SystolicArterialPressure = 120
+                }
+            };
+        }
+
+        public static MonitorRepository Instance
+        {
+            get
+            {
+                if (null != _instance)
+                {
+                    return _instance;
+                }
+                lock (SyncObject)
+                {
+                    if (null == _instance)
+                    {
+                        _instance = new MonitorRepository();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public PatientParams GetPatientParams()
+        {
+            return _patientParams[Index];
+        }
+    }
+}
---
+++ CardMonitor/Core/ThreadAssistant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using MahApps.Metro.Controls;
+
+namespace CardioMonitor.Core
+{
+    /// <summary>
+    /// Помощник для работы с потоками
+    /// </summary>
+    public class ThreadAssistant
+    {
+        private readonly MetroWindow _userControl;
+
+        /// <summary>
+        /// Помощник для работы с потоками
+        /// </summary>
+        public ThreadAssistant(MetroWindow userControl)
+        {
+            _userControl = userControl;
+        }
+
+        /// <summary>
+        /// Выполняет переданный метод в потоке UI пользователя
+        /// </summary>
+        /// <param name="method"></param>
+        public void StartInUIThread(Action method)
+        {
+            _userControl.Dispatcher.BeginInvoke(method);
+        }
+
+    }
+}
