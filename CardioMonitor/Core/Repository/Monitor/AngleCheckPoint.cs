a9bd7719a36fde600bcdb3333297c10e7c831e81
Markelow Maxim <max.markelow@gmail.com>
2015-07-14 00:33:39 +0300

    Pre merge commit; added updating data validation

---
+++ CardioMonitor/Core/Repository/Monitor/AngleCheckPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardioMonitor.Core.Repository.Monitor
+{
+    public class AngleCheckPoint
+    {
+        public bool IsUppingPassed { get; set; }
+
+        public bool IsDowningPassed { get; set; }
+    }
+}
