872199e83ab6b05c0739d9f346a741d862a6129e
Maxim Markelow <max.markelow@gmail.com>
2018-01-20 18:00:27 +0300

    Copied logging projects

---
+++ src/Infrastructure/Logging/Scout.Utils.Logging.ScoutNLog/NLoggerFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NLog;
+using NLogInternal = NLog;
+
+namespace Scout.Utils.Logging.ScoutNLog
+{
+    /// <summary>
+    /// Фабрика по созданию фасада логера NLog
+    /// </summary>
+    public class NLoggerFactory : ILoggerFactory
+    {
+        /// <summary>
+        /// Фабрика по созданию фасада логера NLog
+        /// </summary>
+        /// <param name="configPath">Путь к конфигурационному файла NLog</param>
+        public NLoggerFactory(string configPath)
+        {
+            LogManager.Configuration = new NLogInternal.Config.XmlLoggingConfiguration(configPath);
+        }
+
+        /// <summary>
+        /// Создает логер с заданным именем
+        /// </summary>
+        /// <param name="loggerName">Имя логера</param>
+        /// <returns>Готовый к использованию логер</returns>
+        /// <param name="correlationId"> не используется</param>
+        public ILogger CreateLogger(string loggerName, string correlationId = null)
+        {
+            if (loggerName == null) throw new ArgumentNullException(nameof(loggerName));
+
+            var logger = LogManager.GetLogger(loggerName);
+          
+            return new NLogger(logger);
+        }
+    }
+}
---
+++ src/Infrastructure/Logging/Scout.Utils.Logging.Serilog/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Scout.Utils.Logs.Serilog")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Scout.Utils.Logs.Serilog")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9943e6ab-8905-41df-aba9-d71926cc41bc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.6.0")]
+[assembly: AssemblyFileVersion("0.1.6.0")]
