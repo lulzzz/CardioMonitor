8a1df7559144c1ecac0d1642ee2f783815f88be2
Maxim Markelow <max.markelow@gmail.com>
2018-04-16 00:28:06 +0300

    Added IpEndPoint from string parser

---
+++ src/Infrastructure/CardioMonitor.Infrastructure/IpEndPointParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+
+namespace CardioMonitor.Infrastructure
+{
+    public class IpEndPointParser
+    {
+        public IPEndPoint Parse(string endPoint)
+        {
+            var ep = endPoint.Split(':');
+            if (ep.Length < 2) throw new FormatException("Invalid endpoint format");
+            IPAddress ip;
+            if (ep.Length > 2)
+            {
+                if (!IPAddress.TryParse(string.Join(":", ep, 0, ep.Length - 1), out ip))
+                {
+                    throw new FormatException("Invalid ip-adress");
+                }
+            }
+            else
+            {
+                if (!IPAddress.TryParse(ep[0], out ip))
+                {
+                    throw new FormatException("Invalid ip-adress");
+                }
+            }
+
+            if (!int.TryParse(ep[ep.Length - 1], NumberStyles.None, NumberFormatInfo.CurrentInfo, out var port))
+            {
+                throw new FormatException("Invalid port");
+            }
+            return new IPEndPoint(ip, port);
+        } 
+    }
+}
