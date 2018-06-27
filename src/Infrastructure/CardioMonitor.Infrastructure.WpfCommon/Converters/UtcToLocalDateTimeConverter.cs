c6c24d90ffbe5ace4257d3b500cb711adf0da053
Maxim Markelow <max.markelow@gmail.com>
2018-06-28 00:22:10 +0300

    Added WPF converters for local time and session status

---
+++ src/Infrastructure/CardioMonitor.Infrastructure.WpfCommon/Converters/UtcToLocalDateTimeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CardioMonitor.Infrastructure.WpfCommon.Converters
+{
+    public class UtcToLocalDateTimeConverter : IValueConverter
+    {
+        public object Convert(
+            object value, 
+            Type targetType, 
+            object parameter, 
+            CultureInfo culture)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+            if (DateTime.TryParse(value.ToString(), out var dateTime))
+                throw new ArgumentException($"{nameof(value)} must be {typeof(DateTime)} type");
+
+            return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc).ToLocalTime();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
